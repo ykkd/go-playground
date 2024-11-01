3e6103c63e12ce0b561055c2f3a752b30dc2c4f0
ykkd <ykkd221dev@gmail.com>
2024-11-02 01:03:43 +0900

    feat: basic setup

--- chapter3.go
+++ chapter3.go
@@ -1,1 +1,12 @@
 // Ref: https://docs.google.com/presentation/d/1DtWB-8FcnNb9asxSpIaOLYbAEc9OjBAwMLNxKnPA8pc/edit#slide=id.g4cbe4d134e_0_0
+
+package main
+
+import (
+	fmt
+)
+
+func main() {
+	fmt.Println("chapter 3")
+}
+
