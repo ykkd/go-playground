7eb33b81921ea387b9259b0e3319124982c7e34f
ykkd <ykkd221dev@gmail.com>
2024-11-02 02:49:10 +0900

    feat: implement try2

--- chapter3.go
+++ chapter3.go
@@ -4,12 +4,12 @@ package main
 
 import (
 	"fmt"
+	"slices"
 )
 
 func main() {
 	fmt.Println("chapter 3")
-	try1()
-	composite1()
+	try2()
 }
 
 func try1() {
@@ -66,4 +66,37 @@ func composite1() {
 	fmt.Println(len(array5)) // 5
 
 	fmt.Println(array5[1:2]) // 20 (index1以上、2未満)
+}
+
+func slicesExample() {
+	ns := []int{10, 20, 30, 40, 50}
+
+	fmt.Println(ns[3])
+	fmt.Println(len(ns))
+	fmt.Println(cap(ns)) // 容量
+
+	// 要素追加
+	ns = append(ns, 60, 70) // appendの挙動　容量が足りない場合は元の配列のおよそ2倍の容量を確保し直す
+	fmt.Println(ns, len(ns), cap(ns))
+
+	// capを指定する
+	ms := ns[:4:4]
+	fmt.Println(ms, len(ms), cap(ms))
+
+	// slice tricks
+	ns = append(ns[:3], ns[1:]...)
+	fmt.Println(ns)
+
+	ns = slices.Delete(ns, 1, 3)
+	fmt.Println(ns)
+}
+
+func try2() {
+	slice := []int{19, 86, 1, 12}
+
+	var sum int
+	for i := 0; i < len(slice); i++ {
+		sum += slice[i]
+	}
+	fmt.Println(sum)
 }
